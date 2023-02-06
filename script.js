064e1b4225c62674ff82d5309b9c054130e9f4a8
azariahjerish <azariahjerish100@gmail.com>
2023-02-06 19:35:04 +0530

    changes

--- script.js
+++ script.js
@@ -28,25 +28,7 @@ setInterval(function () {
 
     }, 1000);
 
-setInterval(function () {
-    var date = new Date();
-    var hour = date.getHours();
-    var min = date.getMinutes();
-    var sec = date.getSeconds();
 
-    if (hour < 10) {
-        hour="0"+hour
-    }
-    if (min < 10) {
-        min="0"+min
-    }
-    if (sec < 10) {
-        sec="0"+sec
-    }
-
-    document.getElementById("hrs24").innerHTML=hour+":"+min+":"+sec;
-
-}, 1000);
 
 
 {
@@ -80,33 +62,4 @@ setInterval(function () {
     }) 
 }
 
-var twentyfour = document.getElementById("twentyfour")
-var twelve = document.getElementById("twelve");
 
-twelve.addEventListener("click", (e) => {
-    twelve.setAttribute("style", "display:none")
-    twentyfour.setAttribute("style", "display:block")
-    e.stopPropagation;
-})
-
-twentyfour.addEventListener("click", (e) => {
-    twelve.setAttribute("style", "display:block")
-    twentyfour.setAttribute("style","display:none")
-    e.stopPropagation;
-})
-
-var hrs24 = document.getElementById("hrs24");
-var hrs12 = document.getElementById("hrs12");
-
-
-twelve.addEventListener("click", (e) => {
-    hrs24.setAttribute("style","display:none")
-    hrs12.setAttribute("style", "display:block")
-    e.stopPropagation;
-})
-
-twentyfour.addEventListener("click", (e) => {
-    hrs24.setAttribute("style","display:block")
-    hrs12.setAttribute("style","display:none")
-    e.stopPropagation;
-})
